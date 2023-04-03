738189dfaaccffbc0e34aa74e9caf817c705c230
sophihum <sophia.humps@gmail.com>
2023-04-03 09:29:49 +0200

    first 3 pitches

--- Sophia/Frontend/beanstalk/src/data.js
+++ Sophia/Frontend/beanstalk/src/data.js
@@ -15,9 +15,24 @@ export const experiments = [
 export const pitches = [
     {
         id: 'Jkiw83',
-        name: 'Pitch 1',
-        productDescription: 'This is the first project',
-        teamDescription: 'This is the first team',
-        image: "https://tinuiti.com/wp-content/uploads/legacysitecontent/cpcs/posts_01/2018/10/15033852/funded-with-kickstarter.png",
+        name: 'Bean Odysee',
+        productDescription: 'Bean Odyssey is on a mission to take coffee lovers on a journey around the world through their unique blends. Using only the highest quality beans, they offer a variety of roasts from light to dark, with each blend originating from a different region. Their signature blend features beans from Ethiopia, with hints of dark chocolate and a bold finish',
+        teamDescription: 'The founders are a mix of coffee lovers who met through a coffee industry forum. The team includes David, a coffee roaster and barista with experience in special roasts, Emma, a business development specialist with experience in the coffee industry, and Sophia, a supply chain management specialist with experience in managing logistics for small businesses.',
+        image: "src\lib\images\roast_logo.png",
     },
+    {
+        id: 'Jkiw83',
+        name: 'Fusion Roasters',
+        productDescription: 'Fusion Roasters is all about blending the best beans from around the world to create unique and flavorful roasts. They specialize in medium to dark roasts, with a focus on blends that bring out the best qualities of each bean. Their signature blend features beans from Ethiopia and Brazil, with hints of dark cherry and a bold finish.'        ',
+        teamDescription: 'The founders are strangers who met through a coffee trade fair. The team includes Maria, a coffee roaster and bariata with long term experience in roasting, Claire, a digital marketer with experience in subscription box services, and Rachel, a product designer with a passion for coffee culture.',
+        image: "src\lib\images\roast_logo.png",
+    },
+    {
+        id: 'Jkiw83',
+        name: 'Bold Bean Coffee Company',
+        productDescription: 'We offer a rich and bold roast, using beans sourced from the mountains of South America. Our unique roasting process brings out the full flavor of the beans, resulting in a strong and flavorful cup of coffee.',
+        teamDescription: 'The founders only recently met through a coffee tasting event, discovered their shared passion and decided to go into the coffee business themselves. The team includes Jason, a coffee roaster with experience from light to dark roasts, Tim, a marketing specialist with experience in the coffee industry, and Ryan, a logistics specialist with experience in managing supply chains.',
+        image: "src\lib\images\roast_logo.png",
+    },
+    
 ]
