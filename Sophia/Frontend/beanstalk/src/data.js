a566b7bbb01df10bb1771292b1ec5f87c1930603
sophihum <sophia.humps@gmail.com>
2023-04-30 14:15:40 +0200

    updated descriptions and consent page

--- Sophia/Frontend/beanstalk/src/data.js
+++ Sophia/Frontend/beanstalk/src/data.js
@@ -23,15 +23,12 @@ export const experiments = [
         type: 'SIT in prolific',
         description: '3(friendship ties: three strangers vs two friends and a stranger vs three friends) × 2(funding team identification: low vs high) within-subjects design in a more diverse population sampled via prolific academic. We manipulate ET identification in the team descriptions (i.e., referring to the founding team as “we” and “us” instead of “the founders” and “they”). Here, we ask participants to write a review of the companies listed and to hypothetically allocate a fixed amount of money to the six start-ups.  ',
         pitches: [
-            "F0MI",
-            "F0MA", 
-            "F0FE",
-            "F2MI", 
-            "F2MA", 
-            "F2FE",
-            "F3MI",
-            "F3MA",
-            "F3FE"
+            "S2F0LS",
+            "S2F2LS", 
+            "S2F3LS",
+            "S2F0HS",
+            "S2F2HS", 
+            "S2F3HS",
         ]
     },
     {
@@ -40,21 +37,18 @@ export const experiments = [
         type: 'SIT in business angels',
         description: 'Experiment 3 directly replicates Study 5 in a population of business angels and venture capital investors. In this study, investment in the ET will be operationalised by writing an evaluation of each start-up and by giving an investment recommendation. ',
         pitches: [
-            "F0MI",
-            "F0MA", 
-            "F0FE",
-            "F2MI", 
-            "F2MA", 
-            "F2FE",
-            "F3MI",
-            "F3MA",
-            "F3FE"
+            "S2F0LS",
+            "S2F2LS", 
+            "S2F3LS",
+            "S2F0HS",
+            "S2F2HS", 
+            "S2F3HS",
         ]
     },
 ]
 
 export const pitches = [
-    //Strangers
+    //Strangers Study 1
     {
         id: 'F0MI',
         name: 'Bean Voyage',
@@ -76,7 +70,7 @@ export const pitches = [
         teamDescription: 'The founders Ryan, Jacob, and Nick met this year at a coffee-tasting event. Ryan, the driving force behind Roast Master’s strategic excellence, heads all things operational. Jacob, a branding and marketing guru, develops and executes the company’s messaging and campaigns. And Nick, a first-class barista, made his hobby his profession and roasts incredible coffee blends.',
         image: "https://github.com/cf-lucas-moskau/beanstalk/blob/main/Sophia/Frontend/beanstalk/src/lib/images/roast_logo.png?raw=true",
     },
-    //Two friends
+    //Two friends Study 1
     {
         id: 'F2FE',
         name: 'Terra Roasters',
@@ -98,7 +92,7 @@ export const pitches = [
         teamDescription: 'The founders are coffee-loving friends Samantha and Chris, who found roasting legend Alex to join their venture. Samantha, the creative visionary, crafts marketing campaigns that capture the essence of the company. Chris, the business strategist, keeps a watchful eye on operations, seeking out new opportunities for growth. Alex, the barista and roaster, is always experimenting with new techniques to bring out unique flavours.',
         image: "https://github.com/cf-lucas-moskau/beanstalk/blob/main/Sophia/Frontend/beanstalk/src/lib/images/roast_logo.png?raw=true",
     },
-    //All Friends
+    //All Friends Study 1
     {
         id: 'F3MI',
         name: 'Golden Bean Coffee',
@@ -120,5 +114,58 @@ export const pitches = [
         teamDescription: 'The founders are friends Joseph, Tyler, and William. Joseph, the operational mastermind, oversees logistics and finances with a keen eye for detail. Tyler, the marketing maven, designs campaigns that resonate with customers and make a lasting impression. And William, the resident barista and coffee roaster, sources only the finest beans and roasts them to perfection.',
         image: "https://github.com/cf-lucas-moskau/beanstalk/blob/main/Sophia/Frontend/beanstalk/src/lib/images/roast_logo.png?raw=true",
     },
+    //Stragers Study2+3
+    //Low Soical Identity 
+    {
+        id: 'S2F0LS',
+        name: 'Bean Voyage',
+        productDescription: 'Bean Voyage is a coffee start-up that sources premium coffee beans from women-led farms in Latin America and East Africa. Their roasting process is carefully monitored to ensure a consistent, medium roast with a smooth, velvety finish.',
+        teamDescription: 'We are Kayla, Victoria, and Justin, a team of coffee enthusiasts that met this April through a coffee trade fair and soon founded Origin Roasters. Kayla, our strategic expert, has a knack for identifying growth opportunities for us in the competitive coffee market. Victoria, our talented coffee roaster and barista, brings her extensive experience to each of our roast roasts. And Justin, the driving force behind our company, is in charge of our creative and effective branding. He is creating great campaigns that represent us and how we live our craft.',
+        image: "https://github.com/cf-lucas-moskau/beanstalk/blob/main/Sophia/Frontend/beanstalk/src/lib/images/roast_logo.png?raw=true",
+    },
+    //High Social Identity 
+    {
+        id: 'S2F0HS',
+        name: 'Origin Roasters',
+        productDescription: 'Welcome to Origin Roasters, where we specialize in crafting single-origin, light roasts that are sourced directly from smallholder farmers in Ethiopia, Honduras, and Indonesia. Our commitment flavour is unwavering, and we are dedicated to bringing you the best specialty coffee.',
+        teamDescription: 'The founders of Bean Voyage are coffee lovers David, Emily, and Lucy, who met this spring through a coffee industry forum. David, a coffee roaster and barista, has years of experience in special roasts. Emily, the creative genius, is the person behind the company’s branding and marketing. And Lucy, a seasoned business strategist with a deep understanding of the coffee industry, stirs Bean Voyages operations.',
+        image: "https://github.com/cf-lucas-moskau/beanstalk/blob/main/Sophia/Frontend/beanstalk/src/lib/images/roast_logo.png?raw=true",
+    },
+
+    //Two Friends Study 2+3
+    //Low Social identity 
+    {
+        id: 'S2F2LS',
+        name: 'Terra Roasters',
+        productDescription: 'Terra Roasters is a coffee start-up that specializes in organic, shade-grown coffee from Central and South America. Their dark roast has a bold, smoky flavour with notes of caramel and chocolate.',
+        teamDescription: 'The founders are friends Meghan and Will and their business partner and strategist Hannah. Meghan, a skilled barista and roaster, creates unique blends that awaken the senses. Will, a marketing whiz, crafts the brand’s messaging and campaigns with precision. And, Hannah a seasoned business strategist, leads operations with a sharp eye for growth opportunities.',
+        image: "https://github.com/cf-lucas-moskau/beanstalk/blob/main/Sophia/Frontend/beanstalk/src/lib/images/roast_logo.png?raw=true",
+    },
+    //High Social Identity
+    {
+        id: 'S2F2HS',
+        name: 'Black Magic Roasters',
+        productDescription: 'Introducing Black Magic Roasters, where we specialize in creating coffee blends that are bold and rich in flavor. We source beans from small farms in Ethiopia and undergo a unique roasting process to produce our signature dark roast that will leave you wanting more.',
+        teamDescription: 'We are coffee-loving friends, Mike and Josh, who wanted to make this love our profession and teamed up with marketing expert Rachel to found Black Magic Roasters. Mike, our operational leader, keeps a keen eye on us and ensures smooth logistics and finances. Josh is our resident barista and roaster. Thanks to him, we create the perfect roast profile that brings out the best in our beans. Our newest addition Rachel is the creative force and conceptualizes and executes branding and marketing strategies that tell the story of us and our blends.',
+        image: "https://github.com/cf-lucas-moskau/beanstalk/blob/main/Sophia/Frontend/beanstalk/src/lib/images/roast_logo.png?raw=true",
+    },
+    //All Friends Study 2+3
+    //Low Social Identity 
+    {
+        id: 'S2F3LS',
+        name: 'Golden Bean Coffee',
+        productDescription: 'Golden Bean Coffee specializes in light roast coffee blends using beans from around the world. They focus on highlighting the unique flavours and characteristics of each bean.',
+        teamDescription: 'The founders are friends Jack, Sarah, and Matt. Jack, the barista and coffee roaster, uses his skills to create blends that are both unique and delicious. Sarah, the creative force, develops branding and marketing strategies that showcase Golden Bean’s dedication to the craft. And Matt, the operational leader, ensures that everything runs smoothly behind the scenes.',
+        image: "https://github.com/cf-lucas-moskau/beanstalk/blob/main/Sophia/Frontend/beanstalk/src/lib/images/roast_logo.png?raw=true",
+    },
+    //High Social Identity
+    {
+        id: 'S2F3HS',
+        name: 'Red River Roasters',
+        productDescription: 'Welcome to Red River Roasters! We specialize in medium roast coffee blends sourced from the finest farms in Central and South America. We are using a traditional roasting process brings out the balanced and smooth flavors in each of our carefully crafted cups.',
+        teamDescription: 'We are three friends, Ashley, Beth, and James, whose love of coffee drove us to found Red River Roasters. As the creative mind behind our brand, Ashley leads the charge on developing our branding and marketing campaigns that connect with coffee lovers everywhere. Beth, our resident barista and coffee roaster, puts her passion for unique blends into each of our delicious creations. And James, our business strategist, keeps us on track with his expertise in finance and logistics, ensuring we can continue to deliver the best coffee experience to our customers.',
+        image: "https://github.com/cf-lucas-moskau/beanstalk/blob/main/Sophia/Frontend/beanstalk/src/lib/images/roast_logo.png?raw=true",
+    },
+
     
 ]
