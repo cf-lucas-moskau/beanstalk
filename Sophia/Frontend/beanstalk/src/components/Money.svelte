6fa9ffb0d5009e35140e87211f3afc9039557f16
ekintosch <ekin.yaldiz@cloudfood.de>
2024-02-29 16:11:47 +0100

    handleDelete. FIXME: Add the money back to investment

--- Sophia/Frontend/beanstalk/src/components/Money.svelte
+++ Sophia/Frontend/beanstalk/src/components/Money.svelte
@@ -10,15 +10,23 @@
   }
 
 
+  //FIXME: Add the money back to investment.
+  function handleDelete (index) {
+    investments.update(investmentsArray => {
+    investmentsArray.splice(index, 1);
+    return investmentsArray;
+  });
+  }
+
 </script>
 
 <button class="money-box" on:click={() => (showModal=true)}>
   <div class="amount">Left to invest: {currency}{$investment.toFixed(2)}</div>
   <ModalAlternative bind:showModal>
-    {#each $investments as i}
+    {#each $investments as i, index}
       <p>{i.amount}</p>
-      <input placeholder="{i.amount}" bind:value={i.amount}>
-
+      <input placeholder="new amount" bind:value={i.reason}/>
+      <button on:click={() => handleDelete(i)}>Test delete</button>
     {/each}
 
   </ModalAlternative>
